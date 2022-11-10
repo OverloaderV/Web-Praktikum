e4e37821922c338e652aa4fdec0c08d4b8ecfa5e
Acidmaster99 <Acidmaster99@gmail.com>
2022-11-10 10:53:21 +0100

    changed friends.js to use datalist

--- js/friends.js
+++ js/friends.js
@@ -1,22 +1,28 @@
 var data= 0;
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function () {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        console.log(xmlhttp.responseText)
-        data = JSON.parse(xmlhttp.responseText);
-        console.log(data);
-  
-        let div = document.createElement("div");
-        div.id="ul"
-        document.body.appendChild(div);
-    }
+function getUsers(){
 
-};
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            console.log(xmlhttp.responseText)
+            data = JSON.parse(xmlhttp.responseText);
+            console.log(data);
+            let div = document.createElement("div");
+            div.id="ul"
+            var un = document.getElementById("username");
+            un.append(div);
+        }
+
+    };
 xmlhttp.open("GET", window.chatServer + "/"+ window.chatCollectionID +"/user", true);
 // Add token, e. g., from Tom
 xmlhttp.setRequestHeader('Authorization', 'Bearer '+window.chatToken);
 xmlhttp.send();
+}
 
+
+getUsers()
+/*
 function keyup(){
     let input = document.getElementById("username").value;
     document.getElementById("ul").innerText = '';
@@ -44,4 +50,20 @@ function checkname(){
         alert("User " +document.getElementById("username").value+ " doesn't exist.")
         return false;
     }
+}
+*/
+function initNames(prefix){
+    const datalist = document.getElementById("names");
+    datalist.innerHTML='';
+    for(let name of data){
+        if(prefix === ''||name.startsWith(prefix)){
+            const option = document.createElement('OPTION');
+            option.setAttribute('value',name);
+            datalist.appendChild(option)
+        }
+    }
+}
+function keyupp(input){
+    const text = input.value;
+    initNames(text);
 }
