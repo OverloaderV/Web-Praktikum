60e07d4b5c9727a8008b87698e2de3c47cf6134c
simon-kpf <simon.koepf@hotmail.de>
2022-11-15 12:21:40 +0100

    added UserExists check

--- js/register.js
+++ js/register.js
@@ -9,6 +9,21 @@ function testLength(id, l) {
         inputElement.classList.remove("redborder");
         inputElement.classList.add("greenborder");
     }
+
+var link = 'https://online-lectures-cs.thi.de/chat/c372f16d-94f6-4870-b76d-9be68bcd82f6/user/' + input;
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState == 4) {
+        if(xmlhttp.status == 204) {
+            console.log("Exists");
+            inputElement.classList.add("redborder");
+            inputElement.classList.remove("greenborder");
+        }
+    }
+};
+xmlhttp.open("GET", link, true);
+xmlhttp.send();
 }
 
 function confirm() {
@@ -16,11 +31,16 @@ function confirm() {
     const password = document.getElementById('Pass').value;
     const passwordRepeat = document.getElementById('cPass').value;
 
+    const test = document.getElementById('heading');
+    test.innerText = "test";
+
     if(password === passwordRepeat) {
 
         document.getElementById('cPass').classList.add("greenborder");
+        document.getElementById('cPass').classList.remove("redborder");
     } else {
         document.getElementById('cPass').classList.add("redborder");
+        document.getElementById('cPass').classList.remove("greenborder");
     }
 
 }
