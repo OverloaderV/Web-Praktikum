3a056a02d94e1fa2f59cb3f5b692117810682081
Acidmaster99 <Acidmaster99@gmail.com>
2022-11-30 14:36:12 +0100

    Login v1

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { BackendService } from 'src/app/services/backend.service';
 
 @Component({
     selector: 'app-login',
@@ -7,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-    public constructor() { 
+    //Daten
+    public success:boolean=true;
+    public username:string="";
+    public password:string="";
+
+    public constructor(private router:Router,private backendService:BackendService) { 
     }
 
     public ngOnInit(): void {
     }
 
+    public login():void{
+        this.backendService.login(this.username,this.password)
+        .subscribe((ok) =>{
+            if(ok){
+                console.log("login success");
+                this.router.navigate(["/friends"])
+            }else{
+                this.success=false;
+            }
+        })
+    }
 }
