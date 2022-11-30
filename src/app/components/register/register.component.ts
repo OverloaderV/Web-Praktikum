5b57fc4a94fc2982f8c8a435c8bcf888db235573
Acidmaster99 <Acidmaster99@gmail.com>
2022-11-30 12:38:13 +0100

    3/4 validating checks display

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -7,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+    //Daten
+    public username:string ="";
+    public password:string ="";
+    public cpassword:string ="";
+    public usernameOK:boolean=false;
+    public usernameExists:boolean=false;
+    public passwordOK:boolean=false;
+
     public constructor() { 
     }
 
     public ngOnInit(): void {
+    }
+
+    public validateUsername():void{
+        if (this.username.length >2){
+            this.usernameOK = true;
+        }else{
+            this.usernameOK = false;
+        }
+    }
+
+    public validatePassword():void{
+        if(this.password.length > 7){
+            this.passwordOK = true;
+        }else{
+            this.passwordOK = false;
+        }
+    }
+    public keyUp():void{
+
     }
 
 }
