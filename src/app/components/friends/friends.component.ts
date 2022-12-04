3be573139131b8c27bade5bc5cd67a136395c569
simon-kpf <simon.koepf@hotmail.de>
2022-12-04 12:40:20 +0100

    friends list changes

--- src/app/components/friends/friends.component.ts
+++ src/app/components/friends/friends.component.ts
@@ -1,4 +1,6 @@
 import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { BackendService } from 'src/app/services/backend.service';
 
 @Component({
     selector: 'app-friends',
@@ -11,5 +13,7 @@ export class FriendsComponent implements OnInit {
     }
 
     public ngOnInit(): void {
+
+        this.backendService.loadCurrentUser()
     }
 }
