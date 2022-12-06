f4d8772b46ec4c1f5808b85a21ef7a4dd947383f
Overloader <Overloader@tutanota.com>
2022-12-06 12:25:58 +0100

    onInit

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/User';
+import { Profile } from 'src/app/models/Profile';
+import { BackendService } from 'src/app/services/backend.service';
 
 
 @Component({
@@ -7,9 +10,59 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
-    public constructor() {
+    private user: User | null = null;
+    private profile: Profile | null = null;
+    
+    private fname: HTMLElement | null;
+    private lname: HTMLElement | null;
+    private drink: HTMLSelectElement | null;
+    private aboutme: HTMLElement | null;
+    private oneline: HTMLInputElement | null;
+    private sepline: HTMLInputElement | null;
+
+    public constructor(private backend: BackendService) {
+        this.fname = document.getElementById("fname");
+        this.lname = document.getElementById("lname");
+        this.drink = document.getElementById("drink") as HTMLSelectElement;
+        this.aboutme = document.getElementById("aboutme");
+        this.oneline = document.getElementById("oneline") as HTMLInputElement;
+        this.sepline = document.getElementById("sepline") as HTMLInputElement;
     }
 
     public ngOnInit(): void {
+        this.backend.loadCurrentUser()
+        .subscribe((user: any) => {
+            if (user == null) {
+                console.log("Fehler");
+            } else {
+                this.user = user;
+                this.profile = new Profile(
+                    user.firstName ? user.firstName : '', 
+                    user.lastName ? user.lastName : '',
+                    user.coffeeOrTea ? user.coffeeOrTea : 'neither',
+                    user.description ? user.description : '',
+                    user.chatLayout ? user.chatLayout : ''
+                );
+                if (this.fname != null) {
+                    this.fname.innerText = this.profile.firstName;
+                }
+                if (this.lname != null) {
+                    this.lname.innerText = this.profile.lastName;
+                }
+                if (this.drink != null) {
+                    this.drink.value = this.profile.coffeeOrTea;
+                }
+                if (this.aboutme != null) {
+                    this.aboutme.innerText = this.profile.description;
+                }
+                if (this.oneline != null && this.sepline != null) {
+                    if (this.profile.layout == "oneline") {
+                        this.oneline.select();
+                    } else if (this.profile.layout == "sepline") {
+                        this.sepline.select();
+                    }
+                }
+            }
+        })
     }
 }
