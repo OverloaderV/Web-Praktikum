6f5e4434e847b2b769f94378b97cbfe851d2f89b
Overloader <Overloader@tutanota.com>
2022-12-07 11:07:35 +0100

    Fertig mit dem scheiss

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { BackendService } from 'src/app/services/backend.service';
 import { ContextService } from 'src/app/services/context.service';
+import { Profile } from 'src/app/models/Profile';
 
 
 @Component({
@@ -9,14 +11,41 @@ import { ContextService } from 'src/app/services/context.service';
     styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+    private profile: Profile | null = null;
 
-    public constructor(private context:ContextService,private router:Router) { 
+    public uname = "";
+    public description = "";
+    public drink = "";
+    public name = "";
+
+    public constructor(private backend: BackendService, private context:ContextService,private router:Router) { 
     }
 
     public ngOnInit(): void {
         if(this.context.loggedInUsername==""){
             this.router.navigate(["/login"]);
         }
+        
+        this.backend.loadUser(this.context.currentChatUsername)
+        .subscribe((user: any) => {
+            this.uname = this.context.currentChatUsername;            
+            if (user == null) {
+                console.log("Fehler");
+            } else {
+                this.profile = new Profile(
+                    user.firstName ? user.firstName : '', 
+                    user.lastName ? user.lastName : '',
+                    user.coffeeOrTea ? user.coffeeOrTea : 'neither',
+                    user.description ? user.description : '',
+                    user.layout ? user.layout : ''
+                );
+                
+                this.description = this.profile.description;
+                this.drink = this.profile.coffeeOrTea;
+                this.name = this.profile.firstName.concat(" ").concat(this.profile.lastName);
+            }
+            
+        });
     }
 
 }
